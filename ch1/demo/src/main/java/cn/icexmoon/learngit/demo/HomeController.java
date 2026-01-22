941cfebd9c2a080227af9725f75b2270e61666cb
icexmoon <707488229@qq.com>
2026-01-22 09:44:24 +0800

    add home page

---
+++ ch1/demo/src/main/java/cn/icexmoon/learngit/demo/HomeController.java
@@ -0,0 +1,22 @@
+package cn.icexmoon.learngit.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @ClassName HomeController
+ * @Description
+ * @Author icexmoon@qq.com
+ * @Date 2026/1/22 09:42
+ * @Version 1.0
+ */
+@RestController
+@RequestMapping("/home")
+public class HomeController {
+    @GetMapping("/index")
+    public String index(){
+        return "index";
+    }
+}
