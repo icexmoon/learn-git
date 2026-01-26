b31900fa8a5956ffc4215171baf9836bc576620b
icexmoon <707488229@qq.com>
2026-01-26 15:38:19 +0800

    hello3

--- ch1/demo/src/main/java/cn/icexmoon/learngit/demo/HomeController.java
+++ ch1/demo/src/main/java/cn/icexmoon/learngit/demo/HomeController.java
@@ -40,4 +40,14 @@ public class HomeController {
     public String hello1(){
         return "hello1";
     }
+
+    @GetMapping("/hello2")
+    public String hello2(){
+        return "hello2";
+    }
+
+    @GetMapping("/hello3")
+    public String hello3(){
+        return "hello3";
+    }
 }
