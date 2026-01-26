1f54cb6ce27dddc484a291a6a8c6b7f8ccb6edd6
icexmoon <707488229@qq.com>
2026-01-26 10:25:28 +0800

    add test4 function

--- ch1/demo/src/main/java/cn/icexmoon/learngit/demo/HomeController.java
+++ ch1/demo/src/main/java/cn/icexmoon/learngit/demo/HomeController.java
@@ -34,4 +34,9 @@ public class HomeController {
     public String test3(){
         return "test3";
     }
+
+    @GetMapping("/test4")
+    public String test4(){
+        return "test4";
+    }
 }
