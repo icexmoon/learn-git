26eb547f1dc13719c5f8785e5161d7e988578862
icexmoon <707488229@qq.com>
2026-01-30 15:05:29 +0800

    add classroom

---
+++ ch1/demo/src/main/java/cn/icexmoon/learngit/demo/entity/ClassRoom.java
@@ -0,0 +1,16 @@
+package cn.icexmoon.learngit.demo.entity;
+
+import lombok.Data;
+
+/**
+ * @ClassName ClassRoom
+ * @Description
+ * @Author icexmoon@qq.com
+ * @Date 2026/1/30 14:55
+ * @Version 1.0
+ */
+@Data
+public class ClassRoom {
+    private Long id;
+    private String name;
+}
