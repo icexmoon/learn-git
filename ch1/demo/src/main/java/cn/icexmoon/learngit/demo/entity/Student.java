5651534df01f64cd61b2dca23807c6625c9f3959
icexmoon <707488229@qq.com>
2026-02-04 14:34:44 +0800

    add student entity

---
+++ ch1/demo/src/main/java/cn/icexmoon/learngit/demo/entity/Student.java
@@ -0,0 +1,17 @@
+package cn.icexmoon.learngit.demo.entity;
+
+import lombok.Data;
+
+/**
+ * @ClassName Student
+ * @Description
+ * @Author icexmoon@qq.com
+ * @Date 2026/2/4 14:34
+ * @Version 1.0
+ */
+@Data
+public class Student {
+    private Long id;
+    private String name;
+    private Integer age;
+}
